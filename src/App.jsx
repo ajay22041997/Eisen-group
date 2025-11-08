b30e2c70de21e81af28e710fc3e2a931ff5ea201
ajay22041997 <ajaikulkarni21@gmail.com>
2025-11-08 11:53:43 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import CategoryPage from "./components/CategoryPage";
 import ProductCard from "./components/ProductCard";
 import productsData from "./components/productsData";
 import Programs from './components/Programs'
-import Snapgallery from './components/Snapgallery'
+
 
 
 import  Blogsection  from './components/Blogsection';
@@ -106,7 +106,7 @@ const App = () => {
       
     
       <Blogsection />
-       <Snapgallery />
+    
       <ContactPage />
       <ChatBot/>
       <Footer />
